29634c03d399002ee280306824f649984ca335a7
Tigran Mkrtchyan <tigran@itspace.am>
2019-03-29 11:12:36 +0400

    add user

---
+++ src/main/java/am/itspace/newfeaturesinjava8/model/User.java
@@ -0,0 +1,16 @@
+package am.itspace.newfeaturesinjava8.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private int age;
+
+    private String name;
+
+}
