5e84047231cebf98b93f437dd6d637effbb87ed1
Ara Hovhannisyan <ara.hovhannisyan@menugroup.co.uk>
2019-03-29 10:57:02 +0400

    initialization project

---
+++ src/main/java/am/itspace/newfeaturesinjava8/Newfeaturesinjava8Application.java
@@ -0,0 +1,13 @@
+package am.itspace.newfeaturesinjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newfeaturesinjava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Newfeaturesinjava8Application.class, args);
+    }
+
+}
